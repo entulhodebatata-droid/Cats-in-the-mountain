e3fd7baa53798881500b2a030ffaa369621ce60a
entulhodebatata-droid <entulhodebatata@gmail.com>
2025-08-13 22:00:50 -0300

    Adicionado diagramá

--- trabalhoFACULDADE/TrabalhoJogo/main.py
+++ trabalhoFACULDADE/TrabalhoJogo/main.py
@@ -1,16 +1,5 @@
-import pygame
+from Diagramá.jogo import jogo
 
+Jogo = jogo()
+Jogo.Run()
 
-print("Inicio")
-pygame.init()
-window = pygame.display.set_mode(size=(1400, 700))
-print('fim')
-
-# ----------------------------------
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-# fechar definitivamente a janela
-# ----------------------------------
